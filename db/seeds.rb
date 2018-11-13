f8cced367ff3924f84ca6201d6c77dcf0664843a
fvkramer <krame490@gmail.com>
2018-11-13 15:39:14 -0800

    Create views for cat#index and cat#show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.destroy_all
+
+Cat.create!(birth_date: "02/02/2002", color: "black", name: "Fluffy", sex: "F", description: "Loveable cat: one dollar an hour plus tax")
+
+Cat.create!(birth_date: "2013/10/31", color: "black", name: "Scaredy Cat", sex: "M", description: "Halloween Cat")
+
