226026b4e780d2aba5209bb9494f7c5b4f5c5514
fvkramer <krame490@gmail.com>
2018-11-13 17:55:22 -0800

    Update cats_controller and edit.html.erb with forms

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -12,4 +12,24 @@ class CatsController < ApplicationController
   def new
     render :new
   end
+  
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      # redirect_to :new
+      render :new
+    end 
+  end
+  
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+  
+  private
+  def cat_params
+    params.require(:cat).permit(:name, :sex, :birth_date, :color, :description)
+  end 
 end
